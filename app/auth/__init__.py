db131e7eefeae5eaccd4aba158f7db465ac67de7
Sanju R <40911904-sanjur16msc@users.noreply.replit.com>
2025-04-08 06:48:31 +0000

    Revert

--- app/auth/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from flask import Blueprint
-
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
-
-# Import routes at the bottom to avoid circular imports
-from app.auth import routes  # This should be at the bottom
--- app/utils/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Utilities package initialization.
-
-This module provides access to various utility functions used throughout the application.
-"""
-from app.utils.document_processor import analyze_document, extract_text
